7c585782052a3c232b3a5719a1733130fa863148
Temidire <temidireowoeye@gmail.com>
2024-10-11 15:48:42 +0100

    feat: create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <figure>
+            <img src="" alt="Company Logo" />
+            <h1>Soare</h1>
+        </figure>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>Request Quote</li>
+        </ul>
+        <div></div>
+    </nav>
+  )
+}
+
+export default Navbar
