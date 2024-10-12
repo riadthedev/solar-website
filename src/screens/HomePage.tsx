6f4f6d10b556f2fa36c23f62fad6c8a8c842212b
Temidire <temidireowoeye@gmail.com>
2024-10-12 04:32:21 +0100

    fix: update homepage ui

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,8 +1,25 @@
+import AboutUs from '@/components/home/AboutUs'
+import OurProducts from '@/components/home/OurProducts'
+import Faqs from '@/components/home/Faqs'
+import LatestPost from '@/components/home/LatestPost'
+import OurServices from '@/components/home/OurServices'
+import Testimonials from '@/components/home/Testimonials'
+import VideoPlayer from '@/components/home/VideoPlayer'
+import WhyChooseUs from '@/components/home/WhyChooseUs'
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <main>
+      <AboutUs />
+      <OurServices />
+      <WhyChooseUs />
+      <VideoPlayer />
+      <OurProducts />
+      <Testimonials />
+      <Faqs />
+      <LatestPost />
+    </main>
   )
 }
 
