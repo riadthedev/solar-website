dbc0bbbe4aa67b4d203e8ddc6e429ae9d849bfb0
John Tobi <johnriad.dev@gmail.com>
2024-10-12 11:57:34 -0300

    removed black div and added after pseudo element changed welcome bg to cover not contain

--- src/components/home/Welcome.tsx
+++ src/components/home/Welcome.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
 import { Button } from '../ui/button'
@@ -5,9 +6,8 @@ import { MdArrowOutward } from 'react-icons/md'
 
 const Welcome = () => {
   return (
-    <SectionWrapper classname={`relative bg-[url('/assets/images/welcome-bg.png')] bg-no-repeat bg-contain bg-center min-h-[70rem] flex items-center justify-center`} data-aos='zoom-in-up'>
-        <div className="absolute inset-0 bg-black opacity-50 mx-[2rem]"></div>
-        <div className='relative flex flex-col justify-center items-center max-w-[100rem] gap-y-[5rem] '>
+    <SectionWrapper classname={`relative after:content-['*'] after:absolute after:inset-0 after:bg-black after:opacity-50 after:z-10 bg-[url('/assets/images/welcome-bg.png')] bg-no-repeat bg-cover bg-center min-h-[70rem] flex items-center justify-center`} data-aos='zoom-in-up'>
+        <div className='relative flex flex-col justify-center items-center max-w-[100rem] gap-y-[5rem] z-20'>
             <p className='text-primary text-[1.6rem] leading-[2.56rem]'>Welcome to Solor Roof</p>
             <h1 className='text-[7.2rem] font-semibold text-white text-center'>Empowering your future with solar energy</h1>
             <p className='text-white text-center leading-[2.56rem]'>At SolarBright we're committed to revolutionizing the way you think about energy. Say goodbye to skyrocketing utility bills and hello to a brighter, greener future with our state-of-the-art solar energy solutions</p>
