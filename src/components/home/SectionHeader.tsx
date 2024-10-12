a6f899dbb4c33f6a464b37175dcbfc0bf2e7ef81
Temidire <temidireowoeye@gmail.com>
2024-10-12 04:31:01 +0100

    feat: built re-usable section header component

---
+++ src/components/home/SectionHeader.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+interface SectionHeaderProps {
+    children: React.ReactNode;
+    highlight: string;
+    title: string;
+    hightlightStyle?: string;
+  }
+
+const SectionHeader: React.FC<SectionHeaderProps> = ({highlight, hightlightStyle, title, children}) => {
+    return (
+        <div className='flex justify-between '>
+                <div className='space-y-[1rem] w-1/2'>
+                    <h2 className={`text-[1.2rem] w-fit p-[1rem] ${hightlightStyle}`}>{highlight}</h2>
+                    <p className='text-[5rem] font-bold'>{title}</p>
+                </div>
+                {children}
+        </div>
+    )
+}
+
+export default SectionHeader
