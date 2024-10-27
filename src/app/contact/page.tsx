624d9ecaba95fd822fa05f30b66377327c9f0307
Temidire <temidireowoeye@gmail.com>
2024-10-27 09:21:15 +0100

    feat: built contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,10 @@
+import GetToUs from '@/components/contact-us/GetToUs'
+import ContactPage from '@/screens/ContactPage'
 import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <ContactPage />
   )
 }
 
