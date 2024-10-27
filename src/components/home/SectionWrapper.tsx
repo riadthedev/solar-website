45cd4bee1065055c40994ad2e10b13c832e35aa9
Temidire <temidireowoeye@gmail.com>
2024-10-27 09:23:49 +0100

    fix: add dynamic padding

--- src/components/home/SectionWrapper.tsx
+++ src/components/home/SectionWrapper.tsx
@@ -2,16 +2,19 @@ import React, { ReactNode } from 'react';
 
 interface SectionWrapperProps {
   children: ReactNode;
+  space?: boolean;
   classname?: string;
   ref?: React.RefObject<HTMLDivElement>;
+  px?: string;
+  py?: string;
 }
 
 const SectionWrapper = React.forwardRef<HTMLElement, SectionWrapperProps>(
-  ({children, classname, ...props}, ref) => {
+  ({children, classname, px = 'px-[6.15rem]', py = 'py-[12.8rem]', space = true, ...props}, ref) => {
     return (
       <section 
         ref={ref}
-        className={`px-[6.15rem] py-[12.8rem] text-[1.6rem] relative space-y-[5rem] ${classname}`} 
+        className={` ${px} ${py} text-[1.6rem] relative ${ space && 'space-y-[5rem]'} ${classname} `} 
         {...props}
       >
         {children}
